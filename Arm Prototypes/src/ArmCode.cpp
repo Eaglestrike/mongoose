d53d2e552859882a5688ea8e9d8cb264052db3ba
Ankith Uppunda <auppunda@gmail.com>
2015-01-09 15:39:22 -0800

    I added the calibrate method

--- Arm Prototypes/src/ArmCode.cpp
+++ Arm Prototypes/src/ArmCode.cpp
@@ -7,17 +7,16 @@
 
 #include "ArmCode.h"
 
-ArmCode::ArmCode(int lV, int rV, int lE1, int lE2, int rE1, int rE2, int rB, int lB, float p_1, float i_1, float d_1, float p_2, float i_2, float d_2) :
+ArmCode::ArmCode(int lV, int rV, int lE1, int lE2, int rE1, int rE2, int rB, int lB, float p_1, float i_1, float d_1, float p_2, float i_2, float d_2, float period) :
 leftVic(lV), rightVic(rV),
 leftEnc(lE1, lE2), rightEnc(rE1, rE2),
 rightButton(rB), leftButton(lB)
 {
-	leftEnc.Start();
-	rightEnc.Start();
 
 	// PID Controller
-	control1(p_1, i_1, d_1, leftEnc, leftVic);
-	control2(p_2, i_2, d_2, rightEnc, rightVic);
+	control1(p_1, i_1, d_1, leftEnc, leftVic, period);
+	control2(p_2, i_2, d_2, rightEnc, rightVic, period);
+	calibrate();
 }
 
 void ArmCode::open() {
@@ -26,26 +25,29 @@ void ArmCode::open() {
 
 void ArmCode::closed() {
 
+
 }
 
 void ArmCode::setVictors(double left, double right) {
-	leftVic.set(left);
-	rightVic.set(-right);
+	leftVic.Set(left);
+	rightVic.Set(-right);
 }
+
 void ArmCode::setX(double x) {
 	ArmCode::x = x;
 }
+
 void ArmCode::setDeltaX(double deltaX) {
 	ArmCode::deltaX = deltaX;
 }
+
 void ArmCode::setDeltaAndX(double x, double deltaX) {
 	ArmCode::deltaX = deltaX;
 	ArmCode::x = x;
-}
 
-void ArmCode::move(double x) {
 
 }
+
 void ArmCode::calibrate() {
 	while(!rightButton.Get()) {
 		rightVic.Set(-.2);
@@ -59,6 +61,7 @@ void ArmCode::calibrate() {
 	rightVic.Set(0);
 	leftVic.Set(0);
 }
+
 void ArmCode::reset() {
 	leftEnc.Reset();
 	rightEnc.Reset();
