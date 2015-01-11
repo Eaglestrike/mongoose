72aff1801c2814f2efbd03554f3fb0923a57ec0c
icecube45 <theicecube45@gmail.com>
2015-01-11 15:37:25 -0800

    Started work with the graphics window and some commented out socket code (can't test with roborio until we get a new fuse)

--- HUD/client.py
+++ HUD/client.py
@@ -1,1 +1,16 @@
-print("client")
+import socket
+import pygame
+import threading
+#clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#clientsocket.connect(('10.1.14.2', 5800))
+
+#socketbuffer = ""
+pygame.init()
+screen = pygame.display.set_mode((648, 480))
+pygame.display.set_caption('HUD')
+pygame.mouse.set_visible(0)
+while(True):
+	#readbuffer = readbuffer+clientsocker.recv(1024)
+	#temp = str.split(readbuffer, "\n")
+	#readbuffer=temp.pop( )
+	pass
