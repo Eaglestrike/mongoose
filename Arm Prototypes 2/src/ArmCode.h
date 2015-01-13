b7012a2edafa62b625282e3a25900a7e4352b497
Nathan Larson <natekronos@gmail.com>
2015-01-13 07:20:02 -0800

    Created Arm Prototype 2 dummy test

---
+++ Arm Prototypes 2/src/ArmCode.h
@@ -0,0 +1,58 @@
+#include "WPILib.h"
+#include <iostream>
+/*
+ * ArmCode.h
+ *
+ *  Created on: Jan 7, 2015
+ *      Author: Ankith Uppunda
+ */
+
+#ifndef ARMCODE_H_
+#define ARMCODE_H_
+
+#define DEBUG 1
+
+class ArmCode {
+public:
+	ArmCode(int lV, int rV, int lE1, int lE2, int rE1, int rE2, int lB, int rB, float p_1, float i_1, float d_1, float p_2, float i_2, float d_2);
+	virtual ~ArmCode();
+	void setVictors(double left, double right);
+	void open(double width);
+	void closed(bool tote);
+	void reset();
+	void setX(double x);
+	void setDeltaX(double deltaX);
+	void setDeltaAndX(double x, double deltaX);
+	void move();
+	void calibrate();
+	void setL(double l);
+	void setR(double r);
+	int getRTick();
+	int getLTick();
+
+	// Both are positive from their respective sides
+
+private:
+	/* Hardware */
+	Victor leftVic;
+	Victor rightVic;
+	Encoder leftEnc;
+	Encoder rightEnc;
+	DigitalInput rightButton;
+	DigitalInput leftButton;
+
+	/*   PID Controllers   */
+	PIDController* control1;
+	PIDController* control2;
+
+	/*variables */
+	double x;
+	double deltaX;
+	double shortToteWidth;
+	double containerWidth;
+	double totalLength; //TODO: Find the length of the bar in encoder ticks
+	double safeDistance; //TODO: Find the proper distance to set as the safe distance
+
+};
+
+#endif /* ARMCODE_H_ */
