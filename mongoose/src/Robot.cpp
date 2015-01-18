522612239ce3453aca5c1a8eddea9e216cef0c80
Ankith Uppunda <auppunda@gmail.com>
2015-01-18 15:57:00 -0800

    I finished distance profiles

--- mongoose/src/Robot.cpp
+++ mongoose/src/Robot.cpp
@@ -1,13 +1,20 @@
 #include "WPILib.h"
+#include "DistanceProfile.h"
 
 class Robot: public IterativeRobot
 {
 private:
 	LiveWindow *lw;
+	DistanceProfile *prof;
+	DistanceProfile *prof2;
+	Timer *times;
 
 	void RobotInit()
 	{
 		lw = LiveWindow::GetInstance();
+		prof = new DistanceProfile(5, 20, 10);
+		times = new Timer();
+		prof2 = new DistanceProfile(.01, 13.7, 13);
 	}
 
 	void AutonomousInit()
@@ -22,7 +29,13 @@ private:
 
 	void TeleopInit()
 	{
-
+		times->Start();
+		while(times->Get() < 14) {
+			std::cout<<prof->getSetPoint(times->Get())<< " time : " << times->Get()<<std::endl;
+			std::cout<<"second "<<prof2->getSetPoint(times->Get())<<std::endl;
+			Wait(1);
+		}
+		times->Stop();
 	}
 
 	void TeleopPeriodic()
