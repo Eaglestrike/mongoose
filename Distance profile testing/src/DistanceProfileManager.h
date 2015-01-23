28b933f13c18ca03e2c2b1486aeb812bbd796601
Ben Evans <otterdude97@gmail.com>
2015-01-22 19:16:00 -0800

    I created a distance profile manager

---
+++ Distance profile testing/src/DistanceProfileManager.h
@@ -0,0 +1,25 @@
+/*
+ * DistanceProfileManager.h
+ *
+ *  Created on: Jan 22, 2015
+ *      Author: Team 114
+ */
+#include "DistanceProfile.h"
+#include <iostream>
+#include <vector>
+
+#ifndef DISTANCEPROFILEMANAGER_H_
+#define DISTANCEPROFILEMANAGER_H_
+
+class DistanceProfileManager {
+public:
+	DistanceProfileManager(std::vector<DistanceProfile> dists);
+	virtual ~DistanceProfileManager();
+	double getSetPoint(Timer &time, Encoder &enc);
+	bool isDone = false;
+private:
+	std::vector<DistanceProfile> profs;
+	int currentProf = 0;
+};
+
+#endif /* DISTANCEPROFILEMANAGER_H_ */
