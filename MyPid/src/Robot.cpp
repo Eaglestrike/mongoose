951f5ca19aab4116db060f1879861424baace730
Ankith Uppunda <auppunda@gmail.com>
2015-01-24 16:20:31 -0800

    i did pid for learning

--- MyPid/src/Robot.cpp
+++ MyPid/src/Robot.cpp
@@ -7,6 +7,11 @@ private:
 	Encoder* enc1;
 	Victor* motor;
 	float p = .02;
+	float i = .005;
+	float d = .01;
+	Timer* time;
+	float prevError = 0;
+	float totalError = 0;
 	float setPoint = 0;
 
 	void RobotInit()
@@ -14,6 +19,7 @@ private:
 		lw = LiveWindow::GetInstance();
 		enc1 = new Encoder(0,1);
 		motor = new Victor(1);
+		time = new Timer();
 	}
 	void AutonomousInit()
 	{
@@ -33,8 +39,13 @@ private:
 	void TeleopPeriodic()
 	{
 		while(true) {
+			time->Start();
 			float error = enc1->Get() - getSetPoint();
-			motor->Set(error*p);
+			totalError +=error*time->Get();
+			motor->Set(error*p + d*(error - prevError)/time->Get() + totalError*i);
+			time->Stop();
+			time->Reset();
+			prevError = error;
 		}
 	}
 
