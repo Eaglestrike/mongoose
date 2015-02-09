2c671ea95c019f3359e69223879580a7c7f812c0
Jetson TK1 <otterdude97@gmail.com>
2015-02-09 15:06:22 -0800

    added jetson project and tested webcam

--- JetsonTK1/src/main.cpp
+++ JetsonTK1/src/main.cpp
@@ -1,10 +1,22 @@
 
 
 #include <iostream>
+#include "opencv2/opencv.hpp"
+#include "opencv2/gpu/gpu.hpp"
 
+
+using namespace cv;
+using namespace std;
 
 int main(){
 
-	std::cout << "Hello, Jetson" << std::endl;
+	VideoCapture vc(0);
+	Mat image;
+
+	vc >> image;
+
+	imshow("test", image);
+
+	waitKey(0);
 
 }
