c9a9ec3a1b215d8614dec3c5fe44b646b5c1bbf3
Ankith Uppunda <auppunda@gmail.com>
2015-02-23 22:20:37 -0800

    deleted PID stuff
    we can do that in the robot.cpp file itself

--- mongoose/src/Modules/DriveModule.h
+++ mongoose/src/Modules/DriveModule.h
@@ -9,7 +9,6 @@
 #include "Peripherals/ADXRS453Z.h"
 
 
-
 /*   Linear Functions   */
 class DriveFunction {  // methods in this are defaults
 public:
@@ -92,24 +91,11 @@ public:
 	void reset();
 	double getPower();
 	void resetEncoders();
-
-	double getDriveP();
-	double getDriveD();
-	double getDriveI();
-	void setDrivePID();
-	void setDriveSetpoint(float setpoint);
-	
-	double getAngleP();
-	double getAngleI();
-	double getAngleD();
-	void setAnglePID();
-	void setAngleSetpoint(float setpoint);
-	
 	double getLeftPower();
 	double getRightPower();
 
 private:
-	/*   Hardware Stuff    */
+	/*   Hardware    */
 	Victor* m_Left_Victor_1;
 	Victor* m_Left_Victor_2;
 	Victor* m_Right_Victor_1;
@@ -117,9 +103,7 @@ private:
 	Encoder* m_Left_Encoder;
 	ADXRS453Z* m_Gyro;
 
-	/* Pid Controller */
-	PIDController* m_Drive_Controller;
-	PIDController* m_Angle_Controller;
+	/* Pid */
 
 	/*drive Functions */
 	DriveFunction driveFunc;
