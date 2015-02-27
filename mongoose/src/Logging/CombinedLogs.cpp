2ab5f37d98ab7dc99c0305e8d92100d148119fd9
Nathan Larson <natekronos@gmail.com>
2015-02-27 15:26:49 -0800

    Added some logging ability - not finished

--- mongoose/src/Logging/CombinedLogs.cpp
+++ mongoose/src/Logging/CombinedLogs.cpp
@@ -5,22 +5,33 @@
  *      Author: Nathan
  */
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <vector>
 using namespace std;
 
 #if 0
-
 string folderName;
 vector<Logger> logs;
+int version;
 
 CombinedLogs::CombinedLogs() {
+	ofstream versionLog;
+	versionLog.open("version.txt");
 
+	//Parse Version
+
+	string versionString = "log" + version;
+	system("mkdir \"versionString\"");
 }
 
 void addModule(RobotModule module) {
 	string filePath = folderName + "/" + module.getModuleName();
 	Logger log = new Logger(filePath);
+}
+
+int getVersion() {
+
 }
 
 #endif
