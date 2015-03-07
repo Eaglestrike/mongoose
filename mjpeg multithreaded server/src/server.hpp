5bdeff2bd1ffd3a2b0cab29eea304116142b212f
Ben Evans <otterdude97@gmail.com>
2015-03-07 14:16:16 -0800

    server stuff

--- mjpeg multithreaded server/src/server.hpp
+++ mjpeg multithreaded server/src/server.hpp
@@ -8,12 +8,9 @@
 #ifndef SRC_SERVER_HPP_
 #define SRC_SERVER_HPP_
 
-
-#include <boost/asio.hpp>
-#include <boost/bind.hpp>
-#include <boost/asio/error.hpp>
 #include <vector>
 #include <string>
+#include <sys/socket.h>
 #include <opencv2/opencv.hpp>
 
 class server{
@@ -22,14 +19,8 @@ public:
 	void setImage(cv::Mat image);
 
 private:
-	void handle_accept(const boost::system::error_code& e);
-	void handle_write(const boost::system::error_code &e, std::size_t bytes);
 	std::vector<uchar> matToJpeg(const cv::Mat& image);
 
-	boost::asio::io_service io_service_;
-	boost::asio::ip::tcp::endpoint endpoint_;
-	boost::asio::ip::tcp::acceptor acceptor_;
-	boost::asio::ip::tcp::socket socket_;
 	std::string header;
 	std::vector<uchar> image;
 	std::vector<int> parameters;
