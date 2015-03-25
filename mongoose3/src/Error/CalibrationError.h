dfbbd8fbf89197f9fcfeca12d31937091ef20170
Ben Evans <otterdude97@gmail.com>
2015-03-25 14:21:57 -0700

    commiting a seg fault

---
+++ mongoose3/src/Error/CalibrationError.h
@@ -0,0 +1,20 @@
+/*
+ * CalibrationError.h
+ *
+ *  Created on: Feb 16, 2015
+ *      Author: Team 114
+ */
+
+#ifndef CALIBRATIONERROR_H_
+#define CALIBRATIONERROR_H_
+
+#include "EaglestrikeError.h"
+
+
+class CalibrationError: public EaglestrikeError {
+public:
+	CalibrationError(RobotModule*, std::string, std::string);
+	virtual ~CalibrationError();
+};
+
+#endif /* CALIBRATIONERROR_H_ */
---
+++ mongoose3/src/Peripherals/ADXRS453Z.h
@@ -0,0 +1,55 @@
+
+#ifndef __ADXRS453Z
+#define __ADXRS453Z
+
+#include <WPILib.h>
+#include <semaphore.h>
+#include <stdio.h>
+
+
+class ADXRS453Z: public SensorBase, public PIDSource, public LiveWindowSendable{
+public:
+	ADXRS453Z(SPI::Port);
+	ADXRS453Z(int);
+	ADXRS453Z();
+	~ADXRS453Z();
+	double getAngle();
+	double getAnglePerSecond();
+	double PIDGet();
+	void accumulate();
+	void reset();
+	uint32_t query();
+
+private:
+	Notifier* accumulator;
+	SPI* spi;
+	Timer* timer;
+	sem_t m_semaphore;
+
+	double lastTime;
+	double angle;
+	double adjustment = 0.0902 - .49335;
+
+	int ignoreCounter = 0;
+
+	static SPI::Port intToPort(int);
+	static void callAccumulate(void*);
+	void init(SPI::Port);
+	uint32_t uint8_tTouint32_t(uint8_t*);
+
+public:
+	virtual std::string GetSmartDashboardType();
+	virtual void InitTable(ITable *subtable);
+	virtual void UpdateTable();
+	virtual ITable* GetTable();
+	virtual void StartLiveWindowMode() {}
+	virtual void StopLiveWindowMode() {}
+
+private:
+	ITable* m_table;
+};
+
+
+
+#endif
+
