04fabd6cfd3aba3880dd33d43be57a008021e04a
icecube45 <theicecube45@gmail.com>
2015-03-30 11:24:34 -0700

    Cleaned up HUD code like crazy, implimented color changing background, and matchtime fix.

--- HUD/test.py
+++ HUD/test.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import socket
 HOST="10.1.14.2"
 PORT=5802
@@ -15,49 +15,18 @@ while(True):
 
             #try:
                 
-                readbuffer = readbuffer+s.recv(1024).decode('utf-8')
-               
-              
+                #readbuffer = readbuffer+s.recv(1024).decode('utf-8')
+                readbuffer = s.recv(1024)
+                
                 temp = readbuffer.split("\n")
-         
-                readbuffer=temp.pop( )
-                print temp
+                
+                toprint="test"
+                print(toprint)
+                toprint = "".join(temp)
+                print(toprint)
+                print toprint.split()
             #except:
             #    print("error")
     #except:
     #    sprint("Server not running")
     #    serverstat=False
-=======
-import socket
-HOST="10.1.14.2"
-PORT=5801
-readbuffer = ""
-s=socket.socket( )
-while(True):
-    try:
-        
-        s.connect((HOST, PORT))
-        serverstat = True
-        while(True):
-          
-
-
-            try:
-                
-                readbuffer = readbuffer+s.recv(1024).decode('utf-8')
-               
-              
-                temp = str.split(readbuffer, "\n")
-         
-                readbuffer=temp.pop( )
-                for line in temp:
-                    
-                    line = str.rstrip(line)
-                    line = str.split(line)
-                    print(line)
-            except:
-                print("error")
-    except:
-        #print("Server not running")
-        serverstat=False
->>>>>>> ff84527da1b5c27169665c8984792fe437e3fbcb
